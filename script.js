c72076476651ea9dff329c0bcee89fc3f3d89c18
EL101 <66912828+EL101@users.noreply.github.com>
2020-07-14 21:09:55 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+let currentValue = 0;
+function yo() {
+    currentValue = currentValue*10+1;
+    document.getElementById("div2").innerHTML = currentValue; 
+} 
+function yo2() {
+    currentValue = currentValue*10+2;
+    document.getElementById("div2").innerHTML = currentValue; 
+} 
+function yo3() {
+    currentValue = currentValue*10+3;
+    document.getElementById("div2").innerHTML = currentValue; 
+} 
+function yo4() {
+    currentValue = currentValue*10+4;
+    document.getElementById("div2").innerHTML = currentValue; 
+} 
+function yo5() {
+    currentValue = currentValue*10+5;
+    document.getElementById("div2").innerHTML = currentValue; 
+} 
+function yo6() {
+    currentValue = currentValue*10+6;
+    document.getElementById("div2").innerHTML = currentValue; 
+} 
+function yo7() {
+    currentValue = currentValue*10+7;
+    document.getElementById("div2").innerHTML = currentValue; 
+} 
+function yo8() {
+    currentValue = currentValue*10+8;
+    document.getElementById("div2").innerHTML = currentValue; 
+} 
+function yo9() {
+    currentValue = currentValue*10+9;
+    document.getElementById("div2").innerHTML = currentValue; 
+} 
+function yo0() {
+    currentValue = currentValue*10;
+    document.getElementById("div2").innerHTML = currentValue; 
+} 
