b1e8de3202c0e772b64ff9284c5a514c2eff1a51
EL101 <66912828+EL101@users.noreply.github.com>
2020-08-09 12:45:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,46 +2,80 @@ let currentValue = 0;
 let operator = null; // this is what operation you will be doing like +,-,x,/ 
 let operand1=0;
 let operand2=0;
+let decimal=zero;
 
 function yoPlus() {
-    operand1=currentValue;
-    operator="+";
+    operator = "+";
+    operand1 = currentValue;
+    currentValue = 0;
+}
+function yoMinus() {
+    operator = "-";
+    operand1 = currentValue;
+    currentValue = 0;
+}
+function yoMultiply() {
+    operator = "*"
+    operand1 = currentValue;
+    currentValue = 0;
+}
+function yoDivide() {
+    operator = "/"
+    operand1 = currentValue;
     currentValue = 0;
 }
 function yoEqual() {  // make this all working... 
-    // make it work..
-    // hint: your operand2 is 0 when you hit equal initially, what do you need to do??
+    operand2 = currentValue;
     if (operator === '+') {
         currentValue = operand1 + operand2;
     } else if (operator === '-') {
-        // hw
+        currentValue = operand1 - operand2;
     } else if (operator === '*') {
-        // hw 
+        currentValue = operand1 * operand2; 
     } else if (operator === '/') {
-        // hw
+        currentValue = operand1/operand2;
     } else {
-        // hw harder one... what do you do for all other cases???????????
+        // hw harder one... what do you do for all other cases?
     }
     document.getElementById("div2").innerHTML = currentValue;
     document.getElementById("AC").innerHTML = "C";   
 }
 function yo() {
-    currentValue = currentValue*10+1;
+    if (decimal === non-zero) {
+        currentValue = Number.parseInt(currentValue) + 1*0.1;
+    } else {
+        currentValue = currentValue*10+1;
+    }
     document.getElementById("div2").innerHTML = currentValue;
     document.getElementById("AC").innerHTML = "C"; 
 } 
 function yo2() {
-    currentValue = currentValue*10+2;
+    if (decimal === non-zero) {
+        currentValue =  Number.parseInt(currentValue) + 2*0.1;
+    } else {
+        currentValue = currentValue*10+2;
+    }
+   
     document.getElementById("div2").innerHTML = currentValue; 
     document.getElementById("AC").innerHTML = "C";
 } 
 function yo3() {
-    currentValue = currentValue*10+3;
+    if (decimal === non-zero) {
+        currentValue = Number.parseInt(currentValue) + 3*0.1;
+    } else {
+        currentValue = currentValue*10+3;
+    }
+   
     document.getElementById("div2").innerHTML = currentValue; 
     document.getElementById("AC").innerHTML = "C";
 } 
 function yo4() {
-    currentValue = currentValue*10+4;
+    if (decimal === non-zero) {
+        currentValue = Number.parseInt(currentValue) + 4*0.1;
+    } else {
+        currentValue = currentValue*10+4;
+    }
+    
     document.getElementById("div2").innerHTML = currentValue; 
     document.getElementById("AC").innerHTML = "C";
 } 
@@ -78,6 +112,10 @@ function yoAC() {
     currentValue = 0;
     document.getElementById("div2").innerHTML = currentValue;
     document.getElementById("AC").innerHTML = "AC";
+    operator = null;
+    operand1=0;
+    operand2=0;
+    decimal=zero;
 }
 function yoNegative() {
     currentValue = currentValue*-1;
@@ -88,7 +126,43 @@ function yoPercent() {
     document.getElementById("div2").innerHTML = currentValue;
 }
 
+function yoDecimal() {
+    if (decimal === non-zero){
+        return;
+    }
+    currentValue = currentValue + ".";
+    document.getElementById("div2").innerHTML = currentValue;
+}
+
 function changeAC() {
     let ac = document.getElementById("AC").innerHTML;
 }
 
+// test cases:
+// '1234567890PM' ==> show -123,456,789
+// '0PM' ==> show -0
+// '0.000000PM' ==> show -0.000000
+// '.2xPM' ==> show -0  (*****)
+// '0.' ==> show 0.
+// '.10' ==> show 0.10
+// '1.0' ==> show 1.0
+// '1.02' ==> show 1.02 
+// '1.0.' ==> show 1.0
+// '1.0PM2' ==> show -1.02
+// '1.0PM+' ==> show -1.0
+// '1.0PM++' ==> show -1 (**-1.0)
+// '1.0====' ==> show 1.0
+// '1+2+3=' ==> show 6
+// '1+2+3+' ==> show 6
+// '1+2+3+4=' ==> show 10
+// '1+2+3+4+' ==> show 10 
+// '1+2+3+4+=' ==> show 20
+// '1+2==' ==> 5
+// '1+2+==' ==> 9
+// '1+2+3+4=+=' ==> show 20
+// '2x3==' ==> 18
+// '2x3x=' ==> 36
+// '1+2x3=' ==> 7
+// '1+2x3==' ==> 21
+// '1+2+x3=' ==> 7 (when x is hit, display reverts from 3 to 2) (**)
+// '1+2=.3=' ==> 3.3 ?? iphone shows 2.3 
